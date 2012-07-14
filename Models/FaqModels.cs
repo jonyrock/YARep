d5d3f780f7f5dc0f1887f7710692873eb08cc9cf
Alexey Velikij <gmpota@gmail.com>
2012-07-15 02:34:55 +0400

    FaqModel basic fields

--- Models/FaqModels.cs
+++ Models/FaqModels.cs
@@ -1,1 +1,37 @@
 ﻿
+using System;
+
+class Question {
+
+    private string authorName;
+    public string AuthorName {
+        get { return authorName; }
+        set { authorName = value; }
+    }
+
+    private string authorEmail;
+    public string AuthorEmail {
+        get { return authorEmail; }
+        set { authorEmail = value; }
+    }
+    
+    private string authorPhone;
+    public string AuthorPhone {
+        get { return authorPhone; }
+        set { authorPhone = value; }
+    }
+    
+    private DateTime creationTime;
+    public DateTime CreationTime {
+        get { return creationTime; }
+        set { creationTime = value; }
+    }
+
+    private int id;
+    public int Id {
+        get { return id; }
+        set { id = value; }
+    }
+
+
+}
